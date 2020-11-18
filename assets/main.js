0baf6844cfba764d1ed0d1236dbca96c87995ae1
Ryan Losgano <losganoryan0808@gmail.com>
2020-11-18 19:52:34 +0800

    lightbox

--- assets/main.js
+++ assets/main.js
@@ -1,19 +1,23 @@
 $(document).ready(function(){
-   
     $('.partners-logos').slick({
-        slidesToShow: 6,
+        slidesToShow: 5,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 1000,
+        autoplaySpeed: 1500,
         arrows: false,
         dots: false,
         pushOnHover: false,
         responsive: [{
             breakpoint: 768,
             settings: {
-                slidesToShow: 4,
+                slidesToShow: 3,
                 infinite: true
              },
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3,
+                dots: true
+            }
     }]
     });
 
